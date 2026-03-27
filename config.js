519fab78b62ecfb7602fe609a7e004ec57659cda
ScholesAcg <pakyin109@gmail.com>
2026-03-27 13:56:16 +0800

    config3

--- config.js
+++ config.js
@@ -16,7 +16,7 @@
 //  The Bin ID is safe to commit (the bin is public for reading).
 // =============================================================
 
-const SITE_CONFIG = {
+window.SITE_CONFIG = {
   binId:     "69c6164dc3097a1dd565646f",   // e.g. "64f3a8c712a1b2c3d4e5f6a7"
   masterKey: "$2a$10$hRUg/NvdcZWYTd0mIe5uIOPfy7NFgzk0N0jk6PupginznWfDm7sVK",   // e.g. "$2b$10$abcdef..."  (leave blank if repo is public)
 };
